f3c796e82d679c0c5b6ab36e6545c42cf094df51
Marvin Minder <marvinminder@gmail.com>
2026-02-11 17:25:32 +0100

    add postgres db

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
+extra["flyway.version"] = "11.1.0"
+
 group = "com.zuehlke.academy"
 version = "0.0.1-SNAPSHOT"
 description = "Clean Architecture Demo Project"
@@ -23,10 +25,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-flyway")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.1")
-	implementation("com.h2database:h2")
+	implementation("org.postgresql:postgresql")
+	runtimeOnly("org.flywaydb:flyway-database-postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("com.h2database:h2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 }
 
 tasks.withType<Test> {
